4376430df2bb4d440aad0f6b9dfd04233f4a65c6
Tejas <tejasdange40@gmail.com>
2026-03-03 21:37:15 +0530

    Leetcode 234 Added

---
+++ DSA/src/Leetcode234.java
@@ -0,0 +1,60 @@
+public class Leetcode234 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+
+        public boolean isPalindrome(ListNode head) {
+
+            if (head.next == null) {
+                return true;
+            }
+            if (head == null) {
+                return false;
+            }
+
+            ListNode middle = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null) {
+                middle = middle.next;
+                fast = fast.next.next;
+            }
+
+            ListNode prev = null;
+            ListNode current = middle;
+
+            while (current != null) {
+                ListNode nxt = current.next;
+                current.next = prev;
+                prev = current;
+                current = nxt;
+            }
+
+            ListNode temp = head;
+
+            while (prev != null) {
+                if (temp.val == prev.val) {
+                    prev = prev.next;
+                    temp = temp.next;
+                } else {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+}
