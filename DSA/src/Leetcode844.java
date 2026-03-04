6b081d05e4458f640f8fc2d7a8d77bb70d0c4bb1
Tejas <tejasdange40@gmail.com>
2026-03-04 23:44:14 +0530

    Leetcode 844

---
+++ DSA/src/Leetcode844.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class Leetcode844 {
+    public boolean backspaceCompare(String s, String t) {
+
+        Stack<Character> stack = new Stack<>();
+        Stack<Character> stack2 = new Stack<>();
+
+        for( int i=0; i<s.length(); i++){
+            stack.push(s.charAt(i));
+            if(s.charAt(i)=='#'){
+                stack.pop();
+                if(!stack.isEmpty())
+                    stack.pop();
+            }
+        }
+
+        for(int j=0; j<t.length(); j++){
+            stack2.push(t.charAt(j));
+            if(t.charAt(j)=='#'){
+                stack2.pop();
+                if(!stack2.isEmpty()){
+                    stack2.pop();
+                }
+            }
+        }
+        if(stack.size() != stack2.size()){
+            return false;
+        }
+        while(!stack.isEmpty() && !stack2.isEmpty()){
+            char n = stack.pop();
+            char k = stack2.pop();
+            if(n!=k){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
