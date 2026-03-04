e4459599bc249a123aa98b50b51e16a515fa6b4e
Tejas <tejasdange40@gmail.com>
2026-03-04 23:41:21 +0530

    Leetcode 1295

---
+++ DSA/src/Leetcode1295.java
@@ -0,0 +1,17 @@
+public class Leetcode1295 {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        int digitcount = 0;
+        for(int i =0; i<nums.length; i++){
+            digitcount = 0;
+            while(nums[i]>0){
+                nums[i] =nums[i]/ 10;
+                digitcount ++;
+            }
+            if(digitcount % 2 ==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
