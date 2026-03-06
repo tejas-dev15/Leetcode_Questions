97f0d1c3fbcee6a3287670196a452bca35da9834
Tejas <tejasdange40@gmail.com>
2026-03-06 22:01:39 +0530

    Leetcode 83 Added

---
+++ DSA/src/Leetcode83.java
@@ -0,0 +1,40 @@
+public class Leetcode83 {
+
+      
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
+    class Solution {
+        public ListNode deleteDuplicates(ListNode head) {
+
+            if(head==null){
+                return head;
+            }
+
+            if(head.next == null){
+                return head;
+            }
+
+
+            ListNode current = head;
+            ListNode nxt = head.next;
+
+            while(current.next != null){
+
+                if(current.val == nxt.val){
+                    current.next = nxt.next;
+                    nxt = nxt.next;
+                }else{
+                    current = current.next;
+                    nxt = nxt.next;
+                }
+            }
+            return head;
+        }
+    }
+}
