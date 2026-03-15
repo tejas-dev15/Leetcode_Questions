87457ff0de0ae7bb0836f6f98627bcfb0e32c852
Tejas <tejasdange40@gmail.com>
2026-03-15 20:42:34 +0530

    Leetcode 1472 & 19 Added

---
+++ DSA/src/Leetcode1472.java
@@ -0,0 +1,63 @@
+public class Leetcode1472 {
+    Node head;
+    Node current;
+    class Node{
+        String site;
+        Node next;
+        Node prev;
+
+        Node(String site){
+            this.site = site;
+            next = null;
+            prev = null;
+        }
+    }
+
+    public void BrowserHistory(String homepage) {
+        Node node = new Node(homepage);
+        head = node;
+        current = node;
+    }
+
+    public void visit(String url) {
+        if(head==null){
+            return;
+        }
+
+        Node node = new Node(url);
+        current.next  = node;
+        node.prev = current;
+        current = node;
+        current.next = null;
+    }
+
+    public String back(int steps) {
+        if(steps < 0){
+            return null;
+        }
+
+        for(int i =0; i<steps; i++){
+            if(current.prev != null){
+                current = current.prev;
+            }else{
+                return current.site;
+            }
+        }
+        return current.site;
+    }
+
+    public String forward(int steps){
+        if(steps< 0){
+            return null;
+        }
+
+        for(int i =0; i<steps; i++){
+            if(current.next != null){
+                current = current.next;
+            }else{
+                return current.site;
+            }
+        }
+        return current.site;
+    }
+}
---
+++ DSA/src/Leetcode19.java
@@ -0,0 +1,49 @@
+public class Leetcode19 {
+
+
+      public class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+     
+    class Solution {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+
+            ListNode fast = head;
+            ListNode slow = head;
+
+            if(head==null){
+                return null;
+            }
+
+            if(head.next==null && n==1){
+                return null;
+            }
+
+            for(int i=0; i<n; i++){
+                if(fast!=null){
+                    fast = fast.next;
+                }else{
+                    return head;
+                }
+            }
+            if(fast==null){
+                head= head.next;
+                return head;
+            }
+            while(fast.next != null){
+                slow = slow.next;
+                fast = fast.next;
+            }
+
+
+            slow.next = slow.next.next;
+
+
+            return head;
+        }
+    }
+}
