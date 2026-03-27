5a89588136c76d403334e9225edc485638c5566f
Tejas <tejasdange40@gmail.com>
2026-03-27 23:39:39 +0530

    LeetCode 183 Added

---
+++ DSA/src/String/LeetCode383.java
@@ -0,0 +1,20 @@
+package String;
+
+public class LeetCode383 {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        int freq[] = new int[26];
+
+        for (int i = 0; i< magazine.length(); i++){
+            freq[magazine.charAt(i) - 'a']++;
+        }
+
+        for(int i =0; i < ransomNote.length(); i++){
+            freq[ransomNote.charAt(i) - 'a']--;
+            if(freq[ransomNote.charAt(i) - 'a'] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
