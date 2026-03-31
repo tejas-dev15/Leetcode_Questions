26c596b5426682d8b91aded3671e139d186cebb8
Tejas <tejasdange40@gmail.com>
2026-03-31 23:54:12 +0530

    LeetCode 424 Added

---
+++ DSA/src/HashMaps/LeetCode424.java
@@ -0,0 +1,24 @@
+package HashMaps;
+
+public class LeetCode424 {
+    public int characterReplacement(String s, int k) {
+
+        int left =0;
+
+        int[] freq = new int[26];
+        int maxfreq = 0;
+        int maxLength =0;
+        for(int right=0; right<s.length(); right++){
+            char c = s.charAt(right);
+            freq[c - 'A']++;
+            maxfreq = Math.max(maxfreq, freq[c-'A']);
+
+            while( (right - left + 1) - maxfreq > k){
+                freq[s.charAt(left) - 'A']--;
+                left++;
+            }
+            maxLength = Math.max(maxLength, right-left+1);
+        }
+        return maxLength;
+    }
+}
