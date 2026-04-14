2c9937114c14908ce8221b327cd781e8d05e709d
Tejas <tejasdange40@gmail.com>
2026-04-14 21:24:35 +0530

    LeetCode 287 Added

---
+++ DSA/src/Array/LeetCode287.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class LeetCode287 {
+    public int findDuplicate(int[] nums) {
+
+        int n = nums.length;
+        int slow =nums[0];
+        int fast =nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+
+        slow = nums[0];
+
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+}
