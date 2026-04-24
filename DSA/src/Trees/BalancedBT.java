c7b17c13c993413aa47f834093b8ca5b0799c024
Tejas <tejasdange40@gmail.com>
2026-04-24 23:45:32 +0530

    LeetCode 102, 110, 543 Added

---
+++ DSA/src/Trees/BalancedBT.java
@@ -0,0 +1,36 @@
+package Trees;
+
+public class BalancedBT {
+    public static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+    static class Solution {
+
+        public boolean isBalanced(TreeNode root) {
+            return height(root) != -1;
+        }
+        public int height(TreeNode root){
+            if(root == null) return 0;
+
+            int left = height(root.left);
+            if(left == -1) return -1;
+
+            int right = height(root.right);
+            if(right == -1) return -1;
+
+            if(Math.abs(left - right) > 1) return -1;
+
+            return 1 + Math.max(left, right);
+        }
+    }
+}
---
+++ DSA/src/Trees/DiameterOfBT.java
@@ -0,0 +1,57 @@
+package Trees;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class DiameterOfBT {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public List<List<Integer>> levelOrder(TreeNode root) {
+
+            List<List<Integer>> ans = new ArrayList<>();
+            Queue<TreeNode> q = new LinkedList<>();
+            if (root == null) return ans;
+            q.offer(root);
+
+            while (!q.isEmpty()) {
+
+                int size = q.size();
+
+                List<Integer> level = new ArrayList<>();
+
+                for (int i = 0; i < size; i++) {
+
+                    TreeNode node = q.poll();
+                    level.add(node.val);
+                    if (node.left != null) q.offer(node.left);
+                    if (node.right != null) q.offer(node.right);
+
+                }
+
+                ans.add(level);
+            }
+
+            return ans;
+        }
+    }
+}
